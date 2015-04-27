352cfb4a212b9199814430a9e64fb596d71d190c
Nicolas Mitchell <ncls.mitchell@gmail.com>
2015-04-27 15:20:33 +0200

    correctifs screenshots

--- routes/index.js
+++ routes/index.js
@@ -4,47 +4,47 @@ var app;
 
 //GET home page
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'SAIO - Simplifiez votre relation client digitale' });
 });
 
 //GET knowledge page
 router.get('/base-de-connaissances', function(req, res, next) {
-  res.render('base-de-connaissances');
+  res.render('base-de-connaissances', { title: 'Base de connaissances SAIO - Facilitez les  échanges sur vos différents canaux de<br>communication' });
 });
 
 //GET chat auto page
 router.get('/chat-automatique', function(req, res, next) {
-  res.render('chat-automatique');
+  res.render('chat-automatique', { title: 'Chat automatique SAIO - Désengorgez vos centres de contact et réduisez vos coûts de support' });
 });
 
 //GET live chat page
 router.get('/live-chat', function(req, res, next) {
-  res.render('live-chat');
+  res.render('live-chat', { title: 'Live chat SAIO - Détectez les leads potentiels et augmentez le taux de conversion de votre site Internet' });
 });
 
 //GET business intel page
 router.get('/business-intelligence', function(req, res, next) {
-  res.render('business-intelligence');
+  res.render('business-intelligence', { title: 'Business Intelligence SAIO - Comprenez les besoins et attentes de vos visiteurs pour mieux y répondre' });
 });
 
 //GET society page
 router.get('/societe', function(req, res, next) {
-  res.render('societe');
+  res.render('societe', { title: 'Société SAIO - Découvrez qui se cache derrière SAIO' });
 });
 
 //GET recruitment page
 router.get('/recrutement', function(req, res, next) {
-  res.render('recrutement');
+  res.render('recrutement', { title: 'Recrutement SAIO - Rejoignez l\'aventure SAIO' });
 });
 
 //GET legal mentions page
 router.get('/mentions-legales', function(req, res, next) {
-  res.render('mentions-legales');
+  res.render('mentions-legales', { title: 'Mentions légales SAIO' });
 });
 
 //GET contact page
 router.get('/contact', function(req, res, next) {
-  res.render('contact');
+  res.render('contact', { title: 'Contact SAIO - Contactez-nous !' });
 });
 
 //send mail contact
