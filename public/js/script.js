6fa36f972b3af48a4296d092cf1ef41006cba2ed
Nicolas Mitchell <ncls.mitchell@gmail.com>
2015-06-09 18:06:19 +0200

    page partenaires responsive

--- public/js/script.js
+++ public/js/script.js
@@ -111,6 +111,16 @@ $( document ).ready(function() {
 
   });
 
+  //anchor scroll animation
+  $('a[href^="#"]').click(function(){  
+    var the_id = $(this).attr("href");  
+  
+    $('html, body').animate({  
+        scrollTop:$(the_id).offset().top  
+    }, 'slow');  
+    return false;  
+  });
+
   //ajax
   $(".button.mail").click(function(e){
 
