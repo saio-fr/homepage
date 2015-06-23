1f1d092e8ea90887629c59bfe219428e777d47e3
Nicolas Mitchell <ncls.mitchell@gmail.com>
2015-06-23 18:37:12 +0200

    pricing auto-chat calc

--- public/js/script-pricing.js
+++ public/js/script-pricing.js
@@ -2,10 +2,20 @@ $( document ).ready(function() {
 
 	//variables
 	var forfait = 'basic';
+	var val = 9;
+
 	var calcAuto;
+	var ratioAuto;
+	var conversationRange;
+
 	var calcLive;
-	var ratioAuto = 2;
+	var agents;
 	var ratioLive = 2;
+
+	//slider var
+	var valueBubble = '<output class="rangeslider__value-bubble" />',
+    tempPosition, 
+    position;
 
 	//pricing selection
 	$(".button").click(function() {
@@ -16,12 +26,12 @@ $( document ).ready(function() {
 
 			if ($(this).parent().hasClass("basic")) {
 				forfait = 'basic';
-				calcAuto = calcAuto / ratioAuto;
+				updatePrice();
 				calcLive = calcLive / ratioLive;
 			}
 			if ($(this).parent().hasClass("pro")) {
 				forfait = 'pro';
-				calcAuto = calcAuto / ratioAuto;
+				updatePrice();
 				calcLive = calcLive / ratioLive;
 			}
 			else {
@@ -34,10 +44,41 @@ $( document ).ready(function() {
 		}
 	});
 
+	//update price
+	function updatePrice() {
+    	if (val < 10) {
+			if (forfait === 'basic') {
+				calcAuto = 69 + val * 10;
+			}
+			if (forfait === 'pro') {
+				calcAuto = 79 + val * 20;
+			}
+			$(".price").html(calcAuto + "€ /mois");
+		}
+		if (val > 18) {
+			if (forfait === 'basic') {
+				calcAuto = 519 + (val - 18) * 160;
+			}
+			if (forfait === 'pro') {
+				calcAuto = 599 + (val - 18) * 240;
+			}
+			$(".price").html(calcAuto + "€ /mois");
+		}
+		if ((9 < val) && (val < 19)) {
+			if (forfait === 'basic') {
+				calcAuto = 159 + (val - 9) * 40;
+			}
+			if (forfait === 'pro') {
+				calcAuto = 239 + (val - 9) * 40;
+			}
+			$(".price").html(calcAuto + "€ /mois");
+		}
+		if (val === 28) {
+			$(".price").html("Sur Devis");
+		}
+	}
+
 	//slider
-	var valueBubble = '<output class="rangeslider__value-bubble" />',
-    tempPosition, 
-    position;
 
     //auto chat
 	$(".auto-chat").find('input[type="range"]').rangeslider({
@@ -47,22 +88,47 @@ $( document ).ready(function() {
 	      this.update();
 	  },
 	  onSlide: function(pos, value) {
+	  	val = value;
 	    var $valueBubble = $('.rangeslider__value-bubble', this.$range);
 	    tempPosition = pos + this.grabX;
 	    position = (tempPosition <= this.handleWidth) ? this.handleWidth : (tempPosition >= this.maxHandleX) ? this.maxHandleX : tempPosition;
-	    
-	    if ($valueBubble.length) {
-	      $valueBubble[0].style.left = Math.ceil(position) + 'px';
-	      $valueBubble[0].innerHTML = value +"<br>conversations";
 
-	      if (forfait === 'basic') {
-			calcAuto = value * 0.2 ;	      	
-	      };
-	      if (forfait === 'pro') {
-			calcAuto = value * 0.4 ;	      	
-	      };
-	      $(".count").html(value +" conversations, soit :");
-	      $(".price").html(calcAuto + "€ /mois");
+	    if ($valueBubble.length) {
+	    	$valueBubble[0].style.left = Math.ceil(position) + 'px';
+			if (val < 10) {
+				$valueBubble[0].innerHTML = (value + 1) * 100 +"<br>conversations";
+				if (forfait === 'basic') {
+					calcAuto = 69 + val * 10;
+				}
+				if (forfait === 'pro') {
+					calcAuto = 79 + val * 20;
+				}
+				$(".price").html(calcAuto + "€ /mois");
+			}
+			if (val > 18) {
+				$valueBubble[0].innerHTML = (value - 17) * 10000 +"<br>conversations";
+				if (forfait === 'basic') {
+					calcAuto = 519 + (value - 18) * 160;
+				}
+				if (forfait === 'pro') {
+					calcAuto = 599 + (value - 18) * 240;
+				}
+				$(".price").html(calcAuto + "€ /mois");
+			}
+			if ((9 < val) && (val < 19)) {
+				$valueBubble[0].innerHTML = (val - 8) * 1000 +"<br>conversations";
+				if (forfait === 'basic') {
+					calcAuto = 159 + (val - 9) * 40;
+				}
+				if (forfait === 'pro') {
+					calcAuto = 239 + (val - 9) * 40;
+				}
+				$(".price").html(calcAuto + "€ /mois");
+			}
+			if (value === 28) {
+				$valueBubble[0].innerHTML = "+100000 conversations";
+				$(".price").html("Sur Devis");
+			}
 	    }
 	  }
 	});
@@ -89,7 +155,6 @@ $( document ).ready(function() {
 	      if (forfait === 'pro') {
 			calcLive = value * 0.4 ;	      	
 	      };
-	      $(".count").html(value +" agents, soit :");
 	      $(".price").html(calcLive + "€ /mois");
 	    }
 	  }
